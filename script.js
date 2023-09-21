13e56754626e3a9effa1119513f760be86db5a73
Kenneth <70785144+yulneth@users.noreply.github.com>
2023-09-21 17:51:47 +0800

    added images to the buttons and cleaned up the js

--- script.js
+++ script.js
@@ -6,120 +6,94 @@ let computerWins = 0;
 
 const rock = document.getElementById("rock");
 rock.addEventListener('click',() =>{
-    playerSelection = 'rock';
+    playerSelection = rock.id;
     computerSelection = getComputerChoice(computerChoice);
-    console.log(playerSelection);
-    console.log(computerSelection);
     playRound(playerSelection,computerSelection);
-    if(playerWins ==5){
-        alert('Player Wins');
-    }
-    else if (computerWins ==5){
-        alert('Computer Wins')
-    };
+    determineWinner();
     
 });
 const paper = document.getElementById("paper");
 paper.addEventListener('click',() =>{
-    playerSelection = 'paper';
+    playerSelection = paper.id;
     computerSelection = getComputerChoice(computerChoice);
-    console.log(playerSelection);
-    console.log(computerSelection);
     playRound(playerSelection,computerSelection);
-    if(playerWins ==5){
-        alert('Player Wins');
-    }
-    else if (computerWins ==5){
-        alert('Computer Wins')
-    };
-    
+    determineWinner();
 });
 const scissors = document.getElementById("scissors");
 scissors.addEventListener('click',() =>{
-    playerSelection = 'scissors';
+    playerSelection = scissors.id;
     computerSelection = getComputerChoice(computerChoice);
-    console.log(playerSelection);
-    console.log(computerSelection);
     playRound(playerSelection,computerSelection);
-    if(playerWins ==5){
-        alert('Player Wins');
-    }
-    else if (computerWins ==5){
-        alert('Computer Wins')
-    };
-    
-})
-
-
-
-
-
-
-function getComputerChoice(computerChoice){
-   return (computerChoice[(Math.floor(Math.random() * computerChoice.length))]);
-}
-
+    determineWinner();
+});
 
 function playRound(playerSelection, computerSelection) {
-    if ((playerSelection == "rock") && (computerSelection == "scissors")){
-    playerWins +=1;
-    document.getElementById("result").innerHTML = `Player: ${playerWins} , Computer: ${computerWins}`;
+    if ((playerSelection === "rock") && (computerSelection === "scissors")){
+        playerWins +=1;
+        displayResult();
     
     }
-    else if ((playerSelection == "rock") && (computerSelection == "paper")) { 
+    else if ((playerSelection === "rock") && (computerSelection === "paper")) { 
         computerWins +=1;
-        document.getElementById("result").innerHTML = `Player: ${playerWins} , Computer: ${computerWins}`;
+        displayResult();
     }
-    else if ((playerSelection == "rock") && (computerSelection == "rock")){
+    else if ((playerSelection === "rock") && (computerSelection === "rock")){
     document.getElementById("result").innerHTML = "Draw";
     }
-    else if ((playerSelection == "scissors") && (computerSelection == "paper")) { 
+    else if ((playerSelection === "scissors") && (computerSelection === "paper")) { 
         playerWins +=1;
-        document.getElementById("result").innerHTML = `Player: ${playerWins} , Computer: ${computerWins}`;
+        displayResult();
     }
-    else if ((playerSelection == "scissors") && (computerSelection == "scissors")){
+    else if ((playerSelection === "scissors") && (computerSelection === "scissors")){
     document.getElementById("result").innerHTML = "Draw";
     }
-    else if ((playerSelection == "scissors") && (computerSelection == "rock")) { 
+    else if ((playerSelection === "scissors") && (computerSelection === "rock")) { 
         computerWins +=1;
-        document.getElementById("result").innerHTML = `Player: ${playerWins} , Computer: ${computerWins}`;
+        displayResult();
     }
-    else if ((playerSelection == "paper") && (computerSelection == "rock")) { 
+    else if ((playerSelection === "paper") && (computerSelection === "rock")) { 
         playerWins +=1;
-        document.getElementById("result").innerHTML = `Player: ${playerWins} , Computer: ${computerWins}`;
+        displayResult();
     }
-    else if ((playerSelection == "paper") && (computerSelection == "paper")){
+    else if ((playerSelection === "paper") && (computerSelection === "paper")){
     document.getElementById("result").innerHTML = "Draw";
     }
-    else if ((playerSelection == "paper") && (computerSelection == "scissors")) { 
+    else if ((playerSelection === "paper") && (computerSelection === "scissors")) { 
         computerWins +=1;
-        document.getElementById("result").innerHTML = `Player: ${playerWins} , Computer: ${computerWins}`;
+        displayResult();
     }
     else{
-        document.getElementById("result").innerHTML = "Invalid input";
+        document.getElementById("standing").innerHTML = "Invalid input";
     }
   } 
 
 
-function game(){
-console.log(playRound(playerSelection, computerSelection));
+function displayResult(){
+    document.getElementById("standing").innerHTML = `Player: ${playerWins} , Computer: ${computerWins}`;
+    document.getElementById('result').innerHTML = `Player wins; Computer chose ${computerSelection}`;
 }
 
+function determineWinner(){
+    if(playerWins ===5){
+        alert('Player Wins');
+        playerWins = 0;
+        computerWins =0;
+        document.getElementById("standing").innerHTML = `Player: ${playerWins} , Computer: ${computerWins}`;
+        document.getElementById("result").innerHTML = "";
+    }
+    else if (computerWins ===5){
+        alert('Computer Wins');
+        playerWins = 0;
+        computerWins =0;
+        document.getElementById("standing").innerHTML = `Player: ${playerWins} , Computer: ${computerWins}`;
+        document.getElementById("result").innerHTML = "";
 
-
-
-/*for (let i = 1; i<=5;i++){
-    console.log('game '+ i);
-    playerSelection = prompt("Choose between rock,paper or scissors");
-    playerSelection = playerSelection.toLowerCase();
-    console.log('Player chooses ' + playerSelection);
-    console.log('Machine chooses ' + computerSelection);
-    
-    game();
+    }
 }
-console.log('game over');*/
-
-
+function getComputerChoice(computerChoice){
+    return (computerChoice[(Math.floor(Math.random() * computerChoice.length))]);
+ }
+ 
 
 
 
